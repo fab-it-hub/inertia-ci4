6d5292f38446cfee06fe0d452314cd7ee13b5d7d
Krishna Gujjjar <krishnagujjjar@gmail.com>
2023-12-30 08:46:24 +0530

    build(deps): `php-cs-fixer` config updated
    add override rules & options to find the src folder and set build path

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,10 +2,23 @@
 
 use CodeIgniter\CodingStandard\CodeIgniter4;
 use Nexus\CsConfig\Factory;
+use PhpCsFixer\Finder;
 
-return Factory::create(new CodeIgniter4, [
-    'binary_operator_spaces' => false,
-])->forLibrary(
+$finder = Finder::create()
+    ->in(__DIR__ . '/src/')
+    ->exclude(__DIR__ . '/tests/');
+
+$options = [
+    'finder'    => $finder,
+    'cacheFile' => 'build/.php-cs-fixer.cache'
+];
+
+$overrideRules = [
+    'binary_operator_spaces'            => false,
+    "not_operator_with_successor_space" => false
+];
+
+return Factory::create(new CodeIgniter4, $overrideRules, $options)->forLibrary(
     'Inertia.js Codeigniter 4',
     'Fab IT Hub',
     'hello@fabithub.com',
